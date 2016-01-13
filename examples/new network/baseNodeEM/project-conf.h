0117f38ebb67f8bcc752e1a8247a8eb135a847d5
mattonfarm <matt@onfarmsolutions.com>
2016-01-14 12:55:22 +1300

    Fix UART1 config issue in Basenode

--- examples/new network/baseNodeEM/project-conf.h
+++ examples/new network/baseNodeEM/project-conf.h
@@ -1,3 +1,10 @@
-#define UART0_CONF_BAUD_RATE 38400
 #define NETSTACK_CONF_RDC     nullrdc_driver
 #define DBG_CONF_UART 0
+#define UART0_CONF_BAUD_RATE 38400
+#define UART1_CONF_BAUD_RATE 38400
+
+#define UART1_RX_PORT            GPIO_A_NUM
+#define UART1_RX_PIN             3
+#define UART1_TX_PORT            GPIO_A_NUM
+#define UART1_TX_PIN             2
+
