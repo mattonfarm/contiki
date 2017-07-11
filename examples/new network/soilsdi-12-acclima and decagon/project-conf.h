06070031dfe709c27b683ed365bf0e331767294b
mattonfarm <matt@onfarmsolutions.com>
2017-07-11 12:04:10 +1200

    Break out code for Acclima and Enviropro sensors 	new file:   examples/new network/soilsdi-12-acclima and decagon/Makefile 	new file:   examples/new network/soilsdi-12-acclima and decagon/SDI12.c 	new file:   examples/new network/soilsdi-12-acclima and decagon/project-conf.h 	new file:   examples/new network/soilsdi-12-enviropro/Makefile 	new file:   examples/new network/soilsdi-12-enviropro/enviropro.c 	new file:   examples/new network/soilsdi-12-enviropro/project-conf.h

---
+++ examples/new network/soilsdi-12-acclima and decagon/project-conf.h
@@ -0,0 +1,12 @@
+#define UART0_CONF_BAUD_RATE 38400
+
+#define UART1_CONF_BAUD_RATE 38400
+#define UART1_RX_PORT            GPIO_D_NUM
+#define UART1_RX_PIN             0
+#define UART1_TX_PORT            GPIO_D_NUM
+#define UART1_TX_PIN             1
+
+#define UART1_CONF_UART 1
+
+#define LPM_CONF_ENABLE 0
+//#define DBG_CONF_UART 0
---
+++ examples/new network/soilsdi-12-enviropro/project-conf.h
@@ -0,0 +1,12 @@
+#define UART0_CONF_BAUD_RATE 38400
+
+#define UART1_CONF_BAUD_RATE 38400
+#define UART1_RX_PORT            GPIO_D_NUM
+#define UART1_RX_PIN             4
+#define UART1_TX_PORT            GPIO_D_NUM
+#define UART1_TX_PIN             5
+
+#define UART1_CONF_UART 1
+
+#define LPM_CONF_ENABLE 1
+//#define DBG_CONF_UART 0
